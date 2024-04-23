a30e6b145ec18374d0952b4e8081dba6bc5e4421
Evgeniy Ivanov <john-ik@yandex.ru>
2024-04-23 20:42:00 +0300

    init

---
+++ server/src/main/java/ru/ivanov/march/chat/server/ServerApp.java
@@ -0,0 +1,13 @@
+package ru.ivanov.march.chat.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerApp {
+    public static void main(String[] args) {
+        new Server(8189).start();
+    }
+}
